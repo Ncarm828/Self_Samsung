59fdf0b1a3c5e41b499edeae3851f8b3e6b2fff9
Harsh Patel <harshu195@gmail.com>
2015-07-05 23:22:34 -0400

    Using Animator to load cicular bar

--- app/src/main/java/appsandmaps/temple/edu/self/StepFragment.java
+++ app/src/main/java/appsandmaps/temple/edu/self/StepFragment.java
@@ -15,21 +15,16 @@ import android.widget.TextView;
 
 
 public class StepFragment extends Fragment {
-
-
     TextView tv;
     ProgressBar pBar;
-    int pStatus = 0;
-    int eStatus = 0;
-
-    private Handler handler = new Handler();
-
+    int progress = 0;
+    
     public StepFragment() {
         // Required empty public constructor
     }
 
-    public void setStepCount(Integer stepCount){
-        eStatus = stepCount;
+    public void setStepCount(int stepCount){
+        progress = stepCount;
     }
 
     @Override
@@ -38,43 +33,35 @@ public class StepFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_step, container, false);
 
-
         tv = (TextView) v.findViewById(R.id.tvStep);
+        tv.setText(" # " + "\n" + "steps");
         pBar = (ProgressBar) v.findViewById(R.id.progressBarStep);
-
         pBar.setSecondaryProgress(pBar.getMax());
 
-        new Thread(new Runnable() {
-
+        //using animation to fill the data points
+        ObjectAnimator animator = ObjectAnimator.ofInt(pBar, "progress", progress);
+        //setting the time for 750 miliseconds
+        animator.setDuration(750);
+        animator.setInterpolator(new DecelerateInterpolator());
+        animator.start();
+        //Adding listner to show the endpoint result
+        animator.addListener(new Animator.AnimatorListener() {
             @Override
-            public void run() {
-                // TODO Auto-generated method stub
-                while (pStatus <= eStatus) {
-
-                    handler.post(new Runnable() {
-
-                        @Override
-                        public void run() {
-                            // TODO Auto-generated method stub
-                            pBar.setProgress(pStatus);
-                            tv.setText("" +pStatus + " " +"\n"+"steps" );
-                        }
-                    });
-                    try {
-                        // Sleep for 200 milliseconds.
-                        // Just to display the progress slowly
-                        Thread.sleep(1);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    pStatus++;
-                }
-
+            public void onAnimationStart(Animator animation) {
+                tv.setText(" # " + "\n" + "steps");
             }
-        }).start();
-
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                tv.setText("" + progress + " " + "\n" + "steps");
+            }
+            @Override
+            public void onAnimationCancel(Animator animation) {
+            }
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                tv.setText(" # " + "\n" + "steps");
+            }
+        });
         return v;
     }
-
-
 }
